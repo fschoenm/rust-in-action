18a6a539948b210a53fcea9c8de28a81a7c56cdd
Frank Schoenmann <fschoenm@gmail.com>
2022-08-07 21:44:15 +0200

    Handle STDIN in addition to file input.

--- ch02-grep-lite/src/main.rs
+++ ch02-grep-lite/src/main.rs
@@ -1,8 +1,20 @@
 use clap::{App, Arg};
 use regex::Regex;
 use std::fs::File;
+use std::io;
 use std::io::prelude::*;
 use std::io::BufReader;
+
+fn process_lines<T: BufRead + Sized>(reader: T, re: Regex) {
+    for (line_num, line) in reader.lines().enumerate() {
+        //let contains_substring = re.find(line);
+        let line = line.unwrap();
+        match re.find(&line) {
+            Some(_) => println!("{}:{}", line_num + 1, line),
+            None => (),
+        }
+    }
+}
 
 fn main() {
     // argument handling
@@ -20,23 +32,22 @@ fn main() {
             Arg::with_name("input")
                 .help("The input file to search")
                 .takes_value(true)
-                .required(true),
+                .required(false),
         )
         .get_matches();
 
     let pattern = args.value_of("pattern").unwrap();
     let re = Regex::new(pattern).unwrap();
 
-    let input = args.value_of("input").unwrap();
-    let file = File::open(input).unwrap();
-    let reader = BufReader::new(file);
+    let input = args.value_of("input").unwrap_or("-");
 
-    for (line_num, line) in reader.lines().enumerate() {
-        //let contains_substring = re.find(line);
-        let line = line.unwrap();
-        match re.find(&line) {
-            Some(_) => println!("{}:{}", line_num + 1, line),
-            None => (),
-        }
+    if input == "-" {
+        let stdin = io::stdin();
+        let reader = stdin.lock();
+        process_lines(reader, re);
+    } else {
+        let f = File::open(input).unwrap();
+        let reader = BufReader::new(f);
+        process_lines(reader, re);
     }
 }
