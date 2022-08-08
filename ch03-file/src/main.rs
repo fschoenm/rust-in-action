916c5bf8a20160170cc69664c9026d14442a1fd5
Frank Schoenmann <fschoenm@gmail.com>
2022-08-08 20:38:17 +0200

    Implement methods on File.

--- ch03-file/src/main.rs
+++ ch03-file/src/main.rs
@@ -4,6 +4,28 @@ struct File {
     data: Vec<u8>,
 }
 
+impl File {
+    fn new(name: &str) -> File {
+        File {
+            name: name.into(),
+            data: Vec::new(),
+        }
+    }
+
+    fn new_with_data(name: &str, data: Vec<u8>) -> File {
+        File {
+            name: name.into(),
+            data,
+        }
+    }
+
+    fn read(self: &File, save_to: &mut Vec<u8>) -> usize {
+        save_to.reserve(self.data.len());
+        save_to.append(self.data.clone().as_mut());
+        self.data.len()
+    }
+}
+
 fn open(f: &mut File) -> bool {
     true
 }
@@ -12,22 +34,14 @@ fn close(f: &mut File) -> bool {
     true
 }
 
-fn read(f: &mut File, save_to: &mut Vec<u8>) -> usize {
-    save_to.reserve(f.data.len());
-    save_to.append(f.data.clone().as_mut());
-    f.data.len()
-}
-
 fn main() {
-    let mut f1 = File {
-        name: String::from("f1.txt"),
-        data: vec![114, 117, 115, 116, 33],
-    };
+    let mut f1 = File::new_with_data("f1.txt", vec![114, 117, 115, 116, 33]);
+    let mut _f2 = File::new("f2.txt");
 
     let mut buf: Vec<u8> = vec![];
 
     open(&mut f1);
-    let f1_length = read(&mut f1, &mut buf);
+    let f1_length = f1.read(&mut buf);
     close(&mut f1);
 
     // use buffer
