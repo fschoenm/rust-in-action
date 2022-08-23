9fcb982946ca2d706e6d9f886a9e00509cd2e90e
Frank Schoenmann <fschoenm@gmail.com>
2022-08-23 21:24:43 +0200

    Read input from file instead of static array.

--- ch07-hexdump/src/main.rs
+++ ch07-hexdump/src/main.rs
@@ -1,27 +1,31 @@
+use std::env;
+use std::fs::File;
 use std::io::prelude::*;
 
 const BYTES_PER_LINE: usize = 16;
-const INPUT: &'static [u8]   = br#"
+
 fn main() {
-    println!("Hello, world!");
-}
-"#;
+    let arg1 = env::args().nth(1);
+    let file_name = arg1.expect("usage: hexdump [filename]");
 
-fn main() -> std::io::Result<()> {
-    let mut buffer: Vec<u8> = vec!();
-    INPUT.read_to_end(&mut buffer);
+    let mut f = File::open(&file_name).expect("Unable to open file");
+    let mut pos = 0;
+    let mut buffer = [0; BYTES_PER_LINE];
 
-    let mut position_in_input = 0;
-    for line in buffer.chunks(BYTES_PER_LINE) {
-        print!("[0x{:08x}]", position_in_input);
+    while f.read_exact(&mut buffer).is_ok() {
+        print!("[0x{:08x}]", pos);
 
-        for byte in line {
-            print!(" {:02x}", byte);
+        for byte in &buffer {
+            match *byte {
+                0x00 => print!(".  "),
+                0xff => print!("## "),
+                _ => print!(" {:02x}", byte),
+            }
         }
 
         println!();
-        position_in_input += BYTES_PER_LINE;
+        pos += BYTES_PER_LINE;
     }
 
-    Ok(())
+    // FIXME: [bug] always shows file in 16-byte chunks
 }
